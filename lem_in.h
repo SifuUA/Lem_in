35bd208cefea9b10e3eb2fd285f206ca7400687e
okres <alekseykres@gmail.com>
2017-03-28 17:58:50 +0300

    creat struct

--- lem_in.h
+++ lem_in.h
@@ -1,4 +1,18 @@
 #ifndef LEM_IN_LEM_IN_H
 #define LEM_IN_LEM_IN_H
 
+typedef struct		s_node
+{
+	int ar[1000];
+	struct s_node	*next;
+}					t_str;
+
+typedef struct 		s_list
+{
+	struct s_node	*head;
+	struct s_node	*tail;
+	int				size;
+}					t_list;
+
+
 #endif
--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include "lem_in.h"
 
+
+
 int		main(int argc, char **argv)
 {
-
+	if (argc > 1)
+	{
+		creat_list();
+	}
 }
 
