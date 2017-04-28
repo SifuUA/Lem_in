54aa77b2f79144f3af3f245fe24fba5be326a7a6
okres <alekseykres@gmail.com>
2017-04-28 15:46:59 +0300

    save

--- find_all_roads.c
+++ find_all_roads.c
@@ -53,12 +53,27 @@ void	clear_mark(t_graph *graph)
 	}
 }
 
+int 	get_index(char **str, char *st)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (ft_strcmp(str[i], st) == 0)
+			return (i);
+		i++;
+	}
+	return (0);
+}
 
 void	rec_f(t_all *all, t_graph *graph, t_node *node, t_node *start)
 {
 	t_node *tmp;
+	char **st;
+	int c;
 
-
+	c = 0;
 	if (all->j == 0)
 	{
 		all->res[all->i] = start->begin;
@@ -66,18 +81,22 @@ void	rec_f(t_all *all, t_graph *graph, t_node *node, t_node *start)
 	}
 	if (check_res(ft_strsplit(all->res[all->i], ' '), node->dest))
 		return;
+
+
 	if (ft_strcmp(node->dest, all->end) == 0)
 	{
-		/*clear_mark(graph);
-		start = start->next;
-		node = start;
-		ft_arr_putstr(ft_strsplit(all->res[all->i], ' '));
-		ft_putstr("*****\n");
-		all->i++;*/
+		st = ft_strsplit(all->res[all->i], ' ');
+		all->i++;
+		while (c < get_index(st, node->begin))
+		{
+			all->res[all->i] = ft_strjoin(all->res[all->i], st[c]);
+			c++;
+		}
 		return ;
-		all->res[all->i] = ft_strjoin(all->res[all->i], " ");
-		all->res[all->i] = ft_strjoin(all->res[all->i], node->dest);
 	}
+
+	all->res[all->i] = ft_strjoin(all->res[all->i], " ");
+	all->res[all->i] = ft_strjoin(all->res[all->i], node->dest);
 	while (node)
 	{
 		node->mark = 1;
