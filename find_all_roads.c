7aad5f20616bb3c246bb8bde621daccff059c8df
SifuUA <alekseykres@gmail.com>
2017-05-01 18:42:13 +0300

    change some algho

--- find_all_roads.c
+++ find_all_roads.c
@@ -198,8 +198,9 @@ void    rec_f(t_all *all, t_graph *graph, t_node *node)
 {
 	t_node *tmp;
 
-	if (ft_strcmp(node->dest, all->end) == 0)
-		all->r_rec = 1;
+	//if (ft_strcmp(node->dest, all->end) == 0)
+	//	all->r_rec = 1;
+
 	if (all->j++ == 0)
 	{
 		all->ss = graph->array[find_index(graph->array, node->dest)].head;
@@ -215,8 +216,7 @@ void    rec_f(t_all *all, t_graph *graph, t_node *node)
 	{
 		if (all->r_rec == 1)
 		{
-			//if (ft_strchr_arr(ft_strsplit(all->res[all->i], ' '), all->end))
-				all->i++;
+			all->i++;
 			all->res[all->i] = get_to(all->res[all->i - 1], node->begin);
 		}
 		all->res[all->i] = ft_strjoin(all->res[all->i], " ");
@@ -227,6 +227,9 @@ void    rec_f(t_all *all, t_graph *graph, t_node *node)
 	{
 
 		all->r_rec = 1;
+		//all->k = 1;
+		ft_putstr(all->res[all->i]);
+		ft_putstr("\n");
 		return ;
 	}
 	while (node)
@@ -237,7 +240,8 @@ void    rec_f(t_all *all, t_graph *graph, t_node *node)
 		{
 			tmp = node;
 			all->flag = 0;
-			all->res[all->i] = clear_other(ft_strsplit(all->res[all->i], ' '), node->begin);
+			if (all->r_rec == 1)
+				all->res[all->i] = clear_other(ft_strsplit(all->res[all->i], ' '), node->begin);
 		}
 		node->mark = 1;
 		rec_f(all, graph, tmp);
@@ -249,7 +253,6 @@ void    rec_f(t_all *all, t_graph *graph, t_node *node)
 		{
 			node = all->start_save->next;
 			all->flag = 0;
-			all->i++;
 			all->r_rec = 0;
 			all->res[all->i] = all->start_save->begin;
 			all->res[all->i] = ft_strjoin(all->res[all->i], " ");
